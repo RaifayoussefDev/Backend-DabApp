012ade32eed298d562422e15bb1dac7128ed3f0b
RaifayoussefDev <raifadev@gmail.com>
2025-04-24 18:12:36 +0100

    push

--- database/migrations/2026_04_22_170854_create_bank_cards_table.php
+++ database/migrations/2026_04_22_170854_create_bank_cards_table.php
@@ -15,15 +15,15 @@ return new class extends Migration
             $table->engine = 'InnoDB'; // ✅ Important pour les clés étrangères
 
             $table->id();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade'); // ✅ plus propre et sûr
+            $table->foreignId('user_id')->constrained()->onDelete('cascade'); // 
             $table->string('card_number')->nullable(); // Crypté
             $table->string('card_holder_name')->nullable();
             $table->date('expiration_date')->nullable(); // format: YYYY-MM-DD
             $table->string('cvv')->nullable(); // Chiffré
-            $table->foreignId('card_type_id')->constrained('card_types')->onDelete('restrict'); // ✅ plus clair
+            $table->foreignId('card_type_id')->constrained('card_types')->onDelete('restrict');
             $table->boolean('is_default')->default(false);
             $table->timestamps();
-            
+
         });
     }
 
