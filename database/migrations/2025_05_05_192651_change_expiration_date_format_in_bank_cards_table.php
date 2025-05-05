a7997d06e5fd42c37bbf17a561cff6ae1bf63e08
RaifayoussefDev <raifadev@gmail.com>
2025-05-05 20:35:08 +0100

    push working

--- database/migrations/2025_05_05_192651_change_expiration_date_format_in_bank_cards_table.php
+++ database/migrations/2025_05_05_192651_change_expiration_date_format_in_bank_cards_table.php
@@ -9,17 +9,18 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
+    // In your 2025_05_05_192651_change_expiration_date_format_in_bank_cards_table.php
     public function up()
     {
         Schema::table('bank_cards', function (Blueprint $table) {
-            $table->string('expiration_date', 5)->change();
+            $table->string('expiration_date', 5)->nullable()->change();
         });
     }
 
     public function down()
     {
         Schema::table('bank_cards', function (Blueprint $table) {
-            $table->date('expiration_date')->change();
+            $table->date('expiration_date')->nullable()->change();
         });
     }
 };
