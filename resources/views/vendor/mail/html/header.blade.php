3a2e65d5d13a5390ae826acf8f523cb4340bfb83
yucef raifa <raifadev@gmail.com>
2025-05-05 11:53:20 +0100

    push last day

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,11 +2,11 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Laravel')
-<img src="../../img/mainLogo.png" class="logo" alt="Laravel Logo">
-@else
-{{ $slot }}
-@endif
+    @if (trim($slot) === 'Laravel')
+        <img src="{{ asset('img/mainLogo.png') }}" class="logo" alt="App Logo">
+    @else
+        {{ $slot }}
+    @endif
 </a>
 </td>
 </tr>
