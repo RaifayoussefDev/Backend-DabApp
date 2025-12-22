18b188334d727499a6a5a0712db9f294f853313c
raifa youssef <128696732+RaifayoussefDev@users.noreply.github.com>
2025-12-22 18:46:32 +0100

    licence plate changes

--- app/Http/Controllers/PlateGeneratorController.php
+++ app/Http/Controllers/PlateGeneratorController.php
@@ -180,6 +180,7 @@ class PlateGeneratorController extends Controller
 
             $browsershot = Browsershot::html($html)
                 ->setNodeBinary(env('NODE_BINARY_PATH', '/home/master/.nvm/versions/node/v22.12.0/bin/node'))
+                ->setNodeModulePath('') // Disable global module lookup
                 ->windowSize($windowSize[0], $windowSize[1])
                 ->deviceScaleFactor(3)
                 ->timeout(60)
