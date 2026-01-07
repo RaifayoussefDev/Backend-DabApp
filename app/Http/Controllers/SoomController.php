3ccbe3748c8f1d735fe6f96538be1d628b5ae290
yucef raifa <raifadev@gmail.com>
2026-01-07 13:57:23 +0100

    vehicle_care_other

--- app/Http/Controllers/SoomController.php
+++ app/Http/Controllers/SoomController.php
@@ -979,11 +979,11 @@ class SoomController extends Controller
             ]);
 
             // Mettre à jour le statut du listing à "sold"
-            $submission->listing->update([
-                'status' => 'sold',
-                'closed_at' => now(),
-                'closing_reason' => 'sold_via_soom'
-            ]);
+            // $submission->listing->update([
+            //     // 'status' => 'sold',
+            //     'closed_at' => now(),
+            //     'closing_reason' => 'sold_via_soom'
+            // ]);
 
             // MAINTENANT rejeter tous les autres SOOMs pending pour ce listing
             Submission::where('listing_id', $submission->listing_id)
