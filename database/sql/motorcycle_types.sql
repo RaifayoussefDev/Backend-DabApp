b9e6bab36d2c595ddf8002b904c7e1c81b0b4f70
raifa youssef <128696732+RaifayoussefDev@users.noreply.github.com>
2026-02-03 00:39:31 +0100

    verified

--- database/sql/motorcycle_types.sql
+++ database/sql/motorcycle_types.sql
@@ -3,7 +3,7 @@ INSERT INTO `motorcycle_types` (`id`, `name`) VALUES
 (15, 'Allround'),
 (16, 'Classic'),
 (18, 'Naked bike'),
-(20, 'Sport'),
+(2, 'Sport'),
 (17, 'Sport touring'),
 (19, 'Touring'),
 (21, 'Unspecified category');
