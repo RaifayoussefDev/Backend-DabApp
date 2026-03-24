e26d422dd216df2c87dd9e146d65c6811cdf40d6
yucef raifa <raifadev@gmail.com>
2026-03-24 14:48:53 +0100

    app version

--- app/Http/Controllers/AppVersionController.php
+++ app/Http/Controllers/AppVersionController.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace App\Http\Controllers\Api;
+namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
+use App\Http\Controllers;
 use Illuminate\Http\Request;
 
 class AppVersionController extends Controller
