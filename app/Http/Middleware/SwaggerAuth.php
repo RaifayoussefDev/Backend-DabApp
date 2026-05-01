5e69870edadaa997fe85eba48170255be91b7866
raifa youssef <128696732+RaifayoussefDev@users.noreply.github.com>
2026-05-01 15:15:50 +0100

    test

--- app/Http/Middleware/SwaggerAuth.php
+++ app/Http/Middleware/SwaggerAuth.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class SwaggerAuth
 {
@@ -11,6 +12,15 @@ class SwaggerAuth
     {
         $username = config('swagger-auth.user', env('SWAGGER_USER', 'admin'));
         $password = config('swagger-auth.password', env('SWAGGER_PASSWORD', 'secret'));
+
+        Log::info('SwaggerAuth attempt', [
+            'user_match'     => $request->getUser() === $username,
+            'password_match' => $request->getPassword() === $password,
+            'got_user'       => $request->getUser(),
+            'got_pass'       => $request->getPassword(),
+            'expected_user'  => $username,
+            'expected_pass'  => $password,
+        ]);
 
         if ($request->getUser() != $username || $request->getPassword() != $password) {
             return response('Unauthorized.', 401, ['WWW-Authenticate' => 'Basic']);
