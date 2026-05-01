2db99328df724c0da905637b7db5aa2e53c0e2a2
raifa youssef <128696732+RaifayoussefDev@users.noreply.github.com>
2026-05-01 15:14:33 +0100

    test

--- app/Http/Middleware/SwaggerAuth.php
+++ app/Http/Middleware/SwaggerAuth.php
@@ -9,8 +9,8 @@ class SwaggerAuth
 {
     public function handle(Request $request, Closure $next)
     {
-        $username = env('SWAGGER_USER', 'admin');
-        $password = env('SWAGGER_PASSWORD', 'secret');
+        $username = config('swagger-auth.user', env('SWAGGER_USER', 'admin'));
+        $password = config('swagger-auth.password', env('SWAGGER_PASSWORD', 'secret'));
 
         if ($request->getUser() != $username || $request->getPassword() != $password) {
             return response('Unauthorized.', 401, ['WWW-Authenticate' => 'Basic']);
---
+++ config/swagger-auth.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'user'     => env('SWAGGER_USER', 'admin'),
+    'password' => env('SWAGGER_PASSWORD', 'secret'),
+];
